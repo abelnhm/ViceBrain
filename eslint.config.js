2efbc5a0d4a52cbdc6260b9c1afee971ce03224a
AbeL <abelnhm92@gmail.com>
2026-03-18 15:11:26 +0100

    feat: update lint workflow to verify package.json existence and enhance ESLint configuration

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,16 @@
 module.exports = [
   {
+    languageOptions: {
+      globals: {
+        require: 'readonly',
+        module: 'readonly',
+        __dirname: 'readonly',
+        process: 'readonly',
+        navigator: 'readonly',
+        window: 'readonly',
+        Notification: 'readonly'
+      }
+    },
     files: ['src/**/*.js'],
     rules: {
       'no-unused-vars': 'warn',
